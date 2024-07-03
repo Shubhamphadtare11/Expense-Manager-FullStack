c140ceb9c71c7c17bbb0052d06be3c2e0a623f19
Shubham <shubham.phadtarez11@gmail.com>
2024-07-03 15:52:09 +0530

    linked frontend with backend

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,4 @@
-
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 
 const router = require('express').Router();
