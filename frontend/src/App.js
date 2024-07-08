7c077d16477c27551b6e397040ec79f350828360
Shubham <shubham.phadtarez11@gmail.com>
2024-07-08 14:28:22 +0530

    bg color changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,7 @@ function App() {
 
 const AppStyled = styled.div`
    height: 100vh;
-  background-image: url(${props => props.bg});
+ background-color: black;
   position: relative;
   main{
     flex: 1;
